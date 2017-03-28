bd9c2246cdeb680691b6b97c68c0ca880d5cb471
Eric Stiens <estiens@users.noreply.github.com>
2017-03-28 22:21:44 +0300

    nice copypasta error

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
 # config/initializers/secret_token.rb
-YourApp::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']
+CaterpillarRails::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']
