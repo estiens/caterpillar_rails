6efc67e14fd7586e5a1d63febcfecdb2ff583ba1
Eric Stiens <estiens@users.noreply.github.com>
2017-07-02 14:59:45 -0500

    spacing nit

--- app/models/response.rb
+++ app/models/response.rb
@@ -117,7 +117,7 @@ class Response
     if @substance
       message = "I could tell you want info about #{@substance.name}. "
       message += "I couldn't tell what kind of info. "
-      message += 'You can try info about [substance] or ask another question. I have info about'
+      message += 'You can try info about [substance] or ask another question. I have info about '
       message += 'effects, toxicity, dose information, purity testing, tolerance, safety, or drug interactions.'
     else
       message = complete_unknown_message
