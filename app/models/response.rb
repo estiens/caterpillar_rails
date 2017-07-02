412369dbd2efc0c537a6a4c5ec0bf87ed7a93ae9
Eric Stiens <estiens@users.noreply.github.com>
2017-07-02 14:52:26 -0500

    add more info to default response

--- app/models/response.rb
+++ app/models/response.rb
@@ -116,7 +116,9 @@ class Response
   def could_not_determine_intent
     if @substance
       message = "I could tell you want info about #{@substance.name}. "
-      message += "I couldn't tell what kind of info. Try info, dosage, effects, or testing"
+      message += "I couldn't tell what kind of info. "
+      message += 'You can try info about [substance] or ask another question. I have info about'
+      message += 'effects, toxicity, dose information, purity testing, tolerance, safety, or drug interactions.'
     else
       message = complete_unknown_message
     end
