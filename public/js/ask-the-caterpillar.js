2deba8d4137a16e32c2688784956fa442fbcb0b8
Eric Stiens <estiens@users.noreply.github.com>
2017-07-07 12:06:31 -0500

    submit query on enter button press

--- public/js/ask-the-caterpillar.js
+++ public/js/ask-the-caterpillar.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
-  var $loading = $('#loading').hide();
-  
-  $('#drug-info-button').click(function(){
-    $('#caterpillar-answer').hide();
-    var data = $("#query").val();
-      $.ajax({
-        type: "POST",
-        url: '/query',
-        data: { 'query': data },
-        success: function (data) {
-            var answer = data.data.messages[0].content
-            $("#caterpillar-answer").show();
-            $("#caterpillar-answer").html(answer);
-        }
-        });
-    });
+  $('#loading').hide();
+
+  $(function() {
+  var $selector = $('#query');
+  $(document.body).off('keyup', $selector);
+  $(document.body).on('keyup', $selector, function(event) {
+    if(event.keyCode == 13) { // 13 = Enter Key
+      queryCaterpillar();
+    }
+  });
 });
+
+  $('#drug-info-button').click(function(){
+    queryCaterpillar();
+  });
+});
+
+function queryCaterpillar(){
+  $('#caterpillar-answer').hide();
+  var data = $("#query").val();
+  $.ajax({
+    type: "POST",
+    url: '/query',
+    data: { 'query': data },
+    success: function (data) {
+        var answer = data.data.messages[0].content
+        $("#caterpillar-answer").show();
+        $("#caterpillar-answer").html(answer);
+    }
+  });
+};
